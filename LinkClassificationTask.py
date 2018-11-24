4197bc777de22c336f4e769c37481dcdfa34c73f
ahmed <ahmedtaweel96@gmail.com>
2018-11-24 15:23:54 +0200

    Store model

--- LinkClassificationTask.py
+++ LinkClassificationTask.py
@@ -8,6 +8,10 @@ import re
 import nltk
 from nltk.corpus import stopwords
 from nltk.stem import PorterStemmer
+import pickle
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.model_selection import train_test_split
+from sklearn.naive_bayes import GaussianNB
 
 nltk.download('stopwords')
 
@@ -37,20 +41,24 @@ for i in range(0, len(X)):
     corpus.append(page)
 
 # Creating Bag of Words model
-from sklearn.feature_extraction.text import CountVectorizer
 cv = CountVectorizer(max_features=1500)
 X = cv.fit_transform(corpus).toarray()
 y = dataset.iloc[:, 1].values
 
 # Splitting the dataset into the Training set and Test set
-from sklearn.model_selection import train_test_split
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.20, random_state=0)
 
 # Fitting Naive Bayes to the Training set
-from sklearn.naive_bayes import GaussianNB
 classifier = GaussianNB()
 classifier.fit(X_train, y_train)
 
+# save the model to disk
+filename = 'finalized_model.sav'
+pickle.dump(classifier, open(filename, 'wb'))
+
 # Predicting the Test set results
-y_pred = classifier.predict(X_test)
+loaded_model = pickle.load(open(filename, 'rb'))
+score = loaded_model.score(X_test, y_test)
+
+
 
