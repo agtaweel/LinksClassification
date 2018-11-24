4ffba8f00e25a2e851ef50acddd4a76102da45af
ahmed <ahmedtaweel96@gmail.com>
2018-11-24 04:50:28 +0200

    all dataset

--- LinkClassificationTask.py
+++ LinkClassificationTask.py
@@ -1,8 +1,6 @@
-# Data Preprocessing
+# Data PreProcessing
 
 # Importing the libraries
-import numpy as np
-import matplotlib.pyplot as plt
 import pandas as pd
 from bs4 import BeautifulSoup
 import requests
